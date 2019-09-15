8bd7e56fcb69ff26901c828cebd8fba60e346e93
Raphael <raphaelz@users.noreply.github.com>
2019-09-15 11:00:57 -0400

    Update ViewList.js

--- src/pages/ViewList.js
+++ src/pages/ViewList.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 // components
-import { Container, Card, Button } from "react-bootstrap";
+import { Container, Card, Button, ListGroup } from "react-bootstrap";
 import Item from "../components/Item";
 
 // react
@@ -34,38 +34,35 @@ export default class ViewList extends React.Component {
             </Card>
           )}
         </div>
-        <Card className="mb-2">
-          <Card.Body className="center">
-          <Card.Title>Post Analysis</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">Specifications</Card.Subtitle>
-          <Card.Text>
-            { this.props.state.cart.map((product, idx) => {
+
+        <Card  className="mb-4">
+  <Card.Header>Post Analysis</Card.Header>
+  <ListGroup variant="flush">
+    <ListGroup.Item>
+      <h6 className="text-muted">Specifications</h6>
+    { this.props.state.cart.map((product, idx) => {
               return product.specifications === "" ? 
                 <p>{`${product.model_name} does not meet any building code specifications`}</p>
               : ""
-            }) }
-            </Card.Text>
-            <Card.Subtitle className="mb-2 text-muted">Country of Origin</Card.Subtitle>
-            <Card.Text>
-            { this.props.state.cart.map((product, idx) => {
+            }) }    
+    </ListGroup.Item>
+    <ListGroup.Item>
+      <h6 className="text-muted">Country of Origin</h6>
+    { this.props.state.cart.map((product, idx) => {
               return product.manufacturer.country !== "Canada" ? 
                 <p><strong>{product.model_name}</strong> is not produced in Canada. Consider ordering from a local supplier.</p>
               : ""
-            }) }
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      
+            }) }      
+    </ListGroup.Item>
 
+  </ListGroup>
+</Card>
         <Button
           className="mr-2"
           variant="primary"
           onClick={this.props.gotoSearch}
         >
           Search for Products
-        </Button>
-        <Button className="mr-2" variant="secondary">
-          Post Analysis
         </Button>
         <Button className="mr-2" variant="secondary" download href="/specs.pdf">
           Create PDF
