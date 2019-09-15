373af679584fe4372758660dab16598aa209dd07
Raphael <raphaelz@users.noreply.github.com>
2019-09-15 10:50:40 -0400

    Add Specifications and Country to Post Analysis

--- src/pages/ViewList.js
+++ src/pages/ViewList.js
@@ -34,6 +34,29 @@ export default class ViewList extends React.Component {
             </Card>
           )}
         </div>
+        <Card className="mb-2">
+          <Card.Body className="center">
+          <Card.Title>Post Analysis</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Specifications</Card.Subtitle>
+          <Card.Text>
+            { this.props.state.cart.map((product, idx) => {
+              return product.specifications === "" ? 
+                <p>{`${product.model_name} does not meet any building code specifications`}</p>
+              : ""
+            }) }
+            </Card.Text>
+            <Card.Subtitle className="mb-2 text-muted">Country of Origin</Card.Subtitle>
+            <Card.Text>
+            { this.props.state.cart.map((product, idx) => {
+              return product.manufacturer.country !== "Canada" ? 
+                <p><strong>{product.model_name}</strong> is not produced in Canada. Consider ordering from a local supplier.</p>
+              : ""
+            }) }
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      
+
         <Button
           className="mr-2"
           variant="primary"
