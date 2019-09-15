5c4cce85b4bfb20e4dcb73c0c9a77ed1831e308b
Tony Ko <tony@htko.ca>
2019-09-15 11:05:41 -0400

    Fix specs post analysis not showing up

--- src/pages/ViewList.js
+++ src/pages/ViewList.js
@@ -36,27 +36,26 @@ export default class ViewList extends React.Component {
         </div>
 
         <Card  className="mb-4">
-  <Card.Header>Post Analysis</Card.Header>
-  <ListGroup variant="flush">
-    <ListGroup.Item>
-      <h6 className="text-muted">Specifications</h6>
-    { this.props.state.cart.map((product, idx) => {
-              return product.specifications === "" ? 
-                <p>{`${product.model_name} does not meet any building code specifications`}</p>
-              : ""
-            }) }    
-    </ListGroup.Item>
-    <ListGroup.Item>
-      <h6 className="text-muted">Country of Origin</h6>
-    { this.props.state.cart.map((product, idx) => {
-              return product.manufacturer.country !== "Canada" ? 
-                <p><strong>{product.model_name}</strong> is not produced in Canada. Consider ordering from a local supplier.</p>
-              : ""
-            }) }      
-    </ListGroup.Item>
-
-  </ListGroup>
-</Card>
+          <Card.Header>Post Analysis</Card.Header>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h6 className="text-muted">Specifications</h6>
+              { this.props.state.cart.map((product, idx) => {
+                return !product.specifications.length || !product.specifications[0] || product.specifications[0] === "" ? 
+                  <p className="mb-0"><strong>{product.model_name}</strong> does not meet any building code specifications.</p>
+                : ""
+              }) }    
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h6 className="text-muted">Country of Origin</h6>
+              { this.props.state.cart.map((product, idx) => {
+                return product.manufacturer.country !== "Canada" ? 
+                  <p className="mb-0"><strong>{product.model_name}</strong> is not produced in Canada. Consider ordering from a local supplier.</p>
+                : ""
+              }) }      
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
         <Button
           className="mr-2"
           variant="primary"
